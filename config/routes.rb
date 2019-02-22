e9db1b4ae974e76635fd9f671b3b020758bcdaf3
kajcyk <kajcyk@gmail.com>
2019-02-22 20:33:28 +0100

    Dalsze prace

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
-  get 'fajna_strona', to: 'home#index'
   get 'about', to: 'home#about'
   get 'terms', to: 'home#terms'
+  resources :photos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
