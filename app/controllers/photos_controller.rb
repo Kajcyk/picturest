7697363424c65331f73a162ab16bc719a4486303
kajcyk <kajcyk@gmail.com>
2019-02-26 18:43:56 +0100

    Zakończenie pierwszego etapu - Tworzenie aplikacji

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,10 +1,10 @@
 class PhotosController < ApplicationController
+  before_action :set_photo, only: [:show, :edit, :update, :destroy]
   def index
     @photos = Photo.all
   end
 
   def show
-    @photo = Photo.find(params[:id])
   end
 
   def new
@@ -15,33 +15,39 @@ class PhotosController < ApplicationController
     @photo = Photo.new(photo_params)
 
     if @photo.save
+      flash[:success] = 'Utworzono nowe zdjęcie.'
       redirect_to @photo
     else
+      flash.now[:danger] = 'Nie można utworzyć zdjęcia.'
       render :new
     end
   end
 
   def edit
-    @photo = Photo.find(params[:id])
   end
 
   def update
-    @photo = Photo.find(params[:id])
 
     if @photo.update(photo_params)
+      flash[:success] = 'Aktualizacja przebiegła pomyślnie.'
       redirect_to @photo
     else
+      flash.now[:danger] = 'Nie udało się zaktualizować.'
       render :edit
     end
   end
 
   def destroy
-    @photo = Photo.find(params[:id])
     @photo.destroy
+    flash[:success] = 'Zdjęcie zostało usunięte.'
     redirect_to photos_url
   end
 
 private
+  def set_photo
+    @photo = Photo.find(params[:id])
+  end
+  
   def photo_params
     params.require(:photo).permit(:title, :description, :image)
   end
